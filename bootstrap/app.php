f5421730c21fbd0c608ab5dcd79e3d6a68c6f2d3
phpdeveloper2497 <php2497@gmail.com>
2024-02-21 14:18:45 +0500

    change config cors for frontend2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,9 +11,9 @@
 |
 */
 
-//header('Access-Control-Allow-Origin: *');
-//header('Access-Control-Allow-Methods: POST, OPTIONS,GET,PUT,DELETE');
-//header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, OPTIONS,GET,PUT,DELETE');
+header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization');
 
 
 
