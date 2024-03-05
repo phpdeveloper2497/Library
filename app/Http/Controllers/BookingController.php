641a11ddd48fa2f8111716811fbbcb523a9237ac
phpdeveloper2497 <142909633+phpdeveloper2497@users.noreply.github.com>
2024-03-05 17:46:43 +0500

    chaged

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -41,12 +41,12 @@ class BookingController extends Controller
             $client_id = $request->get('client_id');
             $status = $request->get('status_id');
             foreach ($request->get('books') as $book) {
-                $request->user()->bookings()->create([
-                    'book_id' => $book['book_id'],
-                    'client_id' => $client_id,
-                    'status_id' => $status,
-                    'to' => $book['to']
-                ]);
+//                $request->user()->bookings()->create([
+//                    'book_id' => $book['book_id'],
+//                    'client_id' => $client_id,
+//                    'status_id' => $status,
+//                    'to' => $book['to']
+//                ]);
                 Book::query()->where('id', '=', $book['book_id'])->decrement('quantity');
             }
             return $this->success('Your booking has been made successfully', []);
