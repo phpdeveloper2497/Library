a3f642278492ba2bd23dc89cd052d559fe341867
phpdeveloper2497 <142909633+phpdeveloper2497@users.noreply.github.com>
2024-03-05 17:45:36 +0500

    chaged

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -41,7 +41,7 @@ class BookingController extends Controller
             $client_id = $request->get('client_id');
             $status = $request->get('status_id');
             foreach ($request->get('books') as $book) {
-                $request->user('api')->bookings()->create([
+                $request->user()->bookings()->create([
                     'book_id' => $book['book_id'],
                     'client_id' => $client_id,
                     'status_id' => $status,
