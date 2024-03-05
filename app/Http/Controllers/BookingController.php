78ae037cdbbf61c10a76053d1bada61aad8299c8
phpdeveloper2497 <142909633+phpdeveloper2497@users.noreply.github.com>
2024-03-05 17:47:29 +0500

    chaged

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -40,6 +40,7 @@ class BookingController extends Controller
         if (auth()->user()->hasPermissionTo('booking:create')) {
             $client_id = $request->get('client_id');
             $status = $request->get('status_id');
+            dd($request->user());
             foreach ($request->get('books') as $book) {
 //                $request->user()->bookings()->create([
 //                    'book_id' => $book['book_id'],
